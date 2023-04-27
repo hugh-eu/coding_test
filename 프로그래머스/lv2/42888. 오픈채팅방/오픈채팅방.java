af853193a5d82c4bd02908ce4e6fc9c7fb63a165
Hyeonseung Park <hugh_eu@outlook.com>
2023-04-27 17:12:54 +0900

    [level 2] Title: 오픈채팅방, Time: 126.21 ms, Memory: 164 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList<String> solution(String[] record) {
+        ArrayList<String> answer = new ArrayList<>();
+        Map<String, String> idNick = new HashMap<>();
+        for (String r : record) {
+            if (r.split(" ")[0].equals("Change")) {
+                idNick.put(r.split(" ")[1], r.split(" ")[2]);
+            } else if (r.split(" ")[0].equals("Enter")) {
+                idNick.put(r.split(" ")[1], r.split(" ")[2]);
+            }
+        }
+        for (String r : record) {
+            if (r.split(" ")[0].equals("Enter")) {
+                answer.add(idNick.get(r.split(" ")[1]) + "님이 들어왔습니다.");
+            } else if (r.split(" ")[0].equals("Leave")) {
+                answer.add(idNick.get(r.split(" ")[1]) + "님이 나갔습니다.");
+            }
+        }
+        return answer;
+    }
+}
