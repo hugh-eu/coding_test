52a6d2a8b6ffee609457bc14c2dfe6c9908d3a9b
Hyeonseung Park <hugh_eu@outlook.com>
2023-05-15 17:57:37 +0900

    [unrated] Title: 요격 시스템, Time: 0.50 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] targets) {
+        int answer = 0;
+        
+        Arrays.sort(targets, (o1, o2) -> {
+            return o1[0] - o2[0];
+        });
+        int[] areaMissile = {-1, -1};
+        for (int[] t : targets) {
+            if (areaMissile[0] == -1) {
+                areaMissile[0] = t[0];
+                areaMissile[1] = t[1];
+                answer++;
+            }
+            else {
+                if ((areaMissile[0] <= t[0] && t[0] < areaMissile[1]) || (areaMissile[0] < t[1] && t[1] <= areaMissile[1])) {
+                    areaMissile[0] = Math.max(t[0], areaMissile[0]);
+                    areaMissile[1] = Math.min(t[1], areaMissile[1]);
+                } else {
+                    areaMissile[0] = t[0];
+                    areaMissile[1] = t[1];
+                    answer++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
