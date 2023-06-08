d7adc7a487f58efd69d9aab8f0a7c63748c1dc28
Hyeonseung Park <hugh_eu@outlook.com>
2023-06-08 12:45:24 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 133.64 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -0,0 +1,32 @@
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int cnt = 1;
+            if (i != 0 && i != s.length() - 1) {
+                for (int j = 1; j <= ((s.length() % 2 == 0 ? i < s.length() / 2 : i <= s.length() / 2) ? i : s.length() - 1 - i); j++) {
+                    if (s.charAt(i - j) == s.charAt(i + j)) cnt += 2;
+                    else break;
+                }
+            }
+            if (cnt > answer) answer = cnt;
+        }
+        for (int i = 0; i < s.length() - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                int cnt = 2;
+                if (i != 0 && i != s.length() - 2) {
+                    for (int j = 1; j <= (i < s.length() / 2 ? i : s.length() - 2 - i); j++) {
+                        if (s.charAt(i - j) == s.charAt(i + 1 + j)) cnt += 2;
+                        else break;
+                    }
+                }
+                if (cnt > answer) answer = cnt;
+            }
+        }
+
+        return answer;
+    }
+}
