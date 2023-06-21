9b453e901f3379a55e0b02a5a93925a69a25cfcb
Hyeonseung Park <hugh_eu@outlook.com>
2023-06-21 15:20:45 +0900

    [level 3] Title: 네트워크, Time: 0.18 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,13 +1,29 @@
+import java.util.*;
+
 class Solution {
     private int[] networks;
 
     private int[][] computers;
 
     public void dfs(int com) {
-        for(int i = 0; i < networks.length; i++) {
+        for (int i = 0; i < networks.length; i++) {
             if (networks[i] == 0 && computers[com][i] == 1) {
                 networks[i] = 1;
                 dfs(i);
+            }
+        }
+    }
+
+    public void bfs(int com) {
+        Queue<Integer> q = new ArrayDeque<>();
+        q.offer(com);
+        while (!q.isEmpty()) {
+            int i = q.poll();
+            for (int j = 0; j < networks.length; j++) {
+                if (networks[j] == 0 && computers[i][j] == 1) {
+                    networks[j] = 1;
+                    q.offer(j);
+                }
             }
         }
     }
@@ -19,7 +35,8 @@ class Solution {
         for (int i = 0; i < networks.length; i++) {
             if (networks[i] == 0) {
                 networks[i] = 1;
-                dfs(i);
+//                dfs(i);
+                bfs(i);
                 answer++;
             }
         }
