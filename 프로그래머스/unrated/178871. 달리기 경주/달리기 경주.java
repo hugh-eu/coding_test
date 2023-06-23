0dc89b433aca567b564a1f04436590c12b2d1063
Hyeonseung Park <hugh_eu@outlook.com>
2023-06-23 13:02:33 +0900

    [unrated] Title: 달리기 경주, Time: 0.04 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -2,25 +2,22 @@ import java.util.*;
 
 class Solution {
     private HashMap<String, Integer> idxMap;
-    
-    private String[] players;
 
-    public Solution() {
-        idxMap = new HashMap<>();
-    }
+    private String[] players;
 
     public void call(String name) {
         int idx = idxMap.get(name);
+
         idxMap.put(players[idx - 1], idx);
         idxMap.put(name, idx - 1);
+
         players[idx] = players[idx - 1];
         players[idx - 1] = name;
     }
 
     public String[] solution(String[] players, String[] callings) {
-        for (int i = 0; i < players.length; i++) {
-            idxMap.put(players[i], i);
-        }
+        idxMap = new HashMap<>();
+        for (int i = 0; i < players.length; i++) idxMap.put(players[i], i);
         this.players = players;
 
         for (String name : callings) call(name);
