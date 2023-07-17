0b047e0f1139760903ee2f8c5d091ecf284ad2d4
Hyeonseung Park <hugh_eu@outlook.com>
2023-07-17 16:07:02 +0900

    [unrated] Title: 대충 만든 자판, Time: 11.73 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.java
@@ -4,22 +4,22 @@ class Solution {
 
         for (int i = 0; i < targets.length; i++) {
             // targets[i], 입력할 문자열
-            boolean flagUnable = false;
+            boolean flagUnable = false;  // 입력불가 확인
             int cntTotal = 0;  // 해당 문자열의 입력을 위해 키를 누른 횟수
             for (int j = 0; j < targets[i].length(); j++) {
                 char target = targets[i].charAt(j);  // targets[i]의 각 문자, 입력할 문자
                 boolean flagLoop = true;
-                int cntTouch = 0;  // target 입력을 위해 누른 횟수 - 1
+                int cntTouch = 1;  // target 입력을 위해 누른 횟수
                 while (flagLoop) {
                     int cntOoB = 0;  // 키맵에 남은 문자 확인용
-                    // 각 키맵 j번째 문자와 target을 비교하여 횟수 확인
+                    // 각 키맵을 cntTouch번 눌렀을 때의 문자와 target을 비교
                     for (int k = 0; k < keymap.length; k++) {
-                        if (keymap[k].length() > cntTouch) {
+                        if (keymap[k].length() > cntTouch - 1) {
                             // 타겟과 누른 문자의 일치여부 확인
-                            if (target == keymap[k].charAt(cntTouch)) {
-                                cntTotal += cntTouch + 1;
+                            if (target == keymap[k].charAt(cntTouch - 1)) {
+                                cntTotal += cntTouch;
                                 flagLoop = false;
-                                break;  // 일치시 문자열 총횟수에 합, 다음 문자로
+                                break;  // 일치시 문자열 총 횟수에 합, 다음 문자로
                             }
                         } else cntOoB++;  // 키맵의 길이확인
                         // 키맵 모두 확인, 문자열작성 불가
@@ -31,7 +31,7 @@ class Solution {
                     }
                     cntTouch++;
                 }
-                if (flagUnable) break;
+                if (flagUnable) break;  // 입력 불가시 다음 문자열로
             }
             answer[i] = cntTotal;  // 결과 저장
         }
