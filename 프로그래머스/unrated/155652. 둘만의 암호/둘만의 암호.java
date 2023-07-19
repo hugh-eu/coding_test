76b4419f1356cea42ab659ca5b24404ef87dd412
Hyeonseung Park <hugh_eu@outlook.com>
2023-07-19 14:33:36 +0900

    [unrated] Title: 둘만의 암호, Time: 10.53 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -4,8 +4,8 @@ class Solution {
 
         for (int i = 0; i < s.length(); i++) {
             int tempIndex = index;
-            int j = 0;
             // 처리할 문자와 index 만큼 뒤의 문자 사이에 포함된 skip 수 만큼 index 증가
+            int j = 0;
             while (j != tempIndex) {
                 j++;
                 if (skip.contains(String.valueOf((char)(97 + (((int)s.charAt(i) + j - 97) % 26))))) tempIndex++;  // ASCII
