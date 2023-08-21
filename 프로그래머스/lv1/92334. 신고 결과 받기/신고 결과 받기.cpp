babdee06c221a39063997fbdbb171c87d179e01a
Hyeonseung Park <hugh_eu@outlook.com>
2023-08-21 15:13:35 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.cpp
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.cpp
@@ -8,22 +8,20 @@ using namespace std;
 
 vector<int> solution(vector<string> id_list, vector<string> report, int k) {
     vector<int> answer(id_list.size(), 0);
-    vector<int> cntReported(id_list.size(), 0);
     map<string, vector<string>> reporter;
 
     sort(report.begin(), report.end());
     report.erase(unique(report.begin(), report.end()), report.end());
 
-    for (string r : report) {
+    for (string s : report) {
         vector<string> splitR;
-        stringstream ss(r);
+        stringstream ss(s);
         string temp;
         while(getline(ss, temp, ' ')) splitR.push_back(temp);
         reporter[splitR[1]].push_back(splitR[0]);
-        cntReported[find(id_list.begin(), id_list.end(), splitR[1]) - id_list.begin()]++;
     }
 
-    for (int i = 0; i < cntReported.size(); i++) if (cntReported[i] >= k) for (string id : reporter[id_list[i]]) answer[find(id_list.begin(), id_list.end(), id) - id_list.begin()]++;
+    for (auto r : reporter) if (r.second.size() >= k) for (string s : r.second) answer[find(id_list.begin(), id_list.end(), s) - id_list.begin()]++;
 
     return answer;
 }
