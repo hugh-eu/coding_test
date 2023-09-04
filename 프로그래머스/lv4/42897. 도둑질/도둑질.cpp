8b0e1685757035ea9a8017f16fd65417cf5cbc5c
Hyeonseung Park <hugh_eu@outlook.com>
2023-09-04 14:20:28 +0900

    [level 4] Title: 도둑질, Time: 7.32 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/lv4/42897. 도둑질/도둑질.cpp
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.cpp
@@ -9,13 +9,11 @@ int solution(vector<int> money) {
 
     dp_0_nm2[0] = money[0];
     dp_0_nm2[1] = max(money[0], money[1]);
-    for (int i = 2; i < money.size() - 1; i++) {
-        dp_0_nm2[i] = max(dp_0_nm2[i - 2] + money[i], dp_0_nm2[i - 1]);
-    }
-
     dp_1_nm1[0] = 0;
     dp_1_nm1[1] = money[1];
+
     for (int i = 2; i < money.size(); i++) {
+        dp_0_nm2[i] = max(dp_0_nm2[i - 2] + money[i], dp_0_nm2[i - 1]);
         dp_1_nm1[i] = max(dp_1_nm1[i - 2] + money[i], dp_1_nm1[i - 1]);
     }
 
