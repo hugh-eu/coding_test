6a055c9a6a97e58545b07113fc4e9492e3d0835b
Hyeonseung Park <hugh_eu@outlook.com>
2024-01-07 17:41:08 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.16 ms, Memory: 4.12 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 vector<int> solution(vector<int> arr) {
     if (arr.size() == 1) return {-1};
-    else arr.erase(arr.begin() + (min_element(arr.begin(), arr.end()) - arr.begin()));
+    else arr.erase(min_element(arr.begin(), arr.end()));
 
     return arr;
 }
