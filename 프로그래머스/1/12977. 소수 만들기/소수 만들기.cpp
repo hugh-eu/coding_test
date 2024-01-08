d13ebda6e96d1a43d83fcd7c0a5fd2ec010a131d
Hyeonseung Park <hugh_eu@outlook.com>
2024-01-08 12:15:28 +0900

    [level 1] Title: 소수 만들기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool chkPrime(int n) {
+    for (int i = 2; i <= sqrt(n); i++) if (n % i == 0) return false;
+    return true;
+}
+
+int solution(vector<int> nums) {
+    int answer = 0;
+
+    for (int i = 0; i < nums.size() - 2; i++) {
+        for (int j = i + 1; j < nums.size() - 1; j++) {
+            for (int k = j + 1; k < nums.size(); k++) {
+                if (chkPrime(nums[i] + nums[j] + nums[k])) answer++;
+            }
+        }
+    }
+
+    return answer;
+}
