194461f7c46b5a58361b7f7cb3ca422fd4ccc1de
Hyeonseung Park <hugh_eu@outlook.com>
2024-01-08 17:48:21 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.cpp
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.cpp
@@ -7,16 +7,16 @@ string solution(vector<int> numbers, string hand) {
     string answer = "";
     int leftHand = 10;
     int rightHand = 12;
-    
+
     for (int i : numbers) {
-        if (i == 1 || i == 4 || i == 7) {
+        if (i == 0) i = 11;
+        if ((i - 1) % 3 == 0) {
             answer += 'L';
             leftHand = i;
-        } else if (i == 3 || i == 6 || i == 9) {
+        } else if (i % 3 == 0) {
             answer += 'R';
             rightHand = i;
         } else {
-            if (i == 0) i = 11;
             int distL = (leftHand - 2) % 3 != 0 ? abs(i - (leftHand + 1)) / 3 + 1 : abs(i - leftHand) / 3;
             int distR = (rightHand - 2) % 3 != 0 ? abs(i - (rightHand - 1)) / 3 + 1 : abs(i - rightHand) / 3;
             if (distL > distR) {
