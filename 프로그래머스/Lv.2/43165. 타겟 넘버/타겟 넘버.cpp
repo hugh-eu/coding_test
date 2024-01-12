9d04a7ef1c2ab0156a749b07dac09aae8be222b1
Hyeonseung Park <hugh_eu@outlook.com>
2024-01-12 13:34:45 +0900

    [level Lv.2] Title: 타겟 넘버, Time: 0.07 ms, Memory: 4.02 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+using namespace std;
+
+void dfs(vector<int> &numbers, int &target, int &numsSize, int &answer, int sumValue, int i) {
+    if (i == numsSize) {
+        if (sumValue == target) answer++;
+    } else {
+        for (int j = 0; j < 2; j++) {
+            if (j == 0) dfs(numbers, target, numsSize, answer, sumValue + numbers[i], i + 1);
+            else dfs(numbers, target, numsSize, answer, sumValue - numbers[i], i + 1);
+        }
+    }
+}
+
+int solution(vector<int> numbers, int target) {
+    int numsSize = numbers.size();    
+    int answer = 0;
+    int sumValue = 0;
+    int i = 0;
+
+    dfs(numbers, target, numsSize, answer, sumValue, i);
+    
+    return answer;
+}
