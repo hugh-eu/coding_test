fcbf3556fc31552ce5c99a90e1cb1c5e74ce9146
Hyeonseung Park <hugh_eu@outlook.com>
2024-03-26 13:11:29 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 vector<int> solution(int brown, int yellow) {    
     for (int i = 1; i <= yellow; i++) for (int j = i; j > 0; j--) {
-        if (i * j == yellow && (i + 2) * 2 + j * 2 == brown) {
+        if (i * j == yellow && (i + j + 2) * 2 == brown) {
             return vector<int>({i + 2, j + 2});
         }
     }
