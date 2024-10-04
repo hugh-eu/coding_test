29e3c5bdca9c2b189abbe0cd365a774ee121e48e
Hyeonseung Park <hugh_eu@outlook.com>
2024-10-04 17:56:07 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.cpp
@@ -8,7 +8,7 @@ string solution(string my_string, string overwrite_string, int s) {
     
     answer += my_string.substr(0, s);
     answer += overwrite_string;
-    answer += my_string.substr(s + overwrite_string.size());
+    if (s + overwrite_string.size() < my_string.size()) answer += my_string.substr(s + overwrite_string.size());
     
     return answer;
 }
