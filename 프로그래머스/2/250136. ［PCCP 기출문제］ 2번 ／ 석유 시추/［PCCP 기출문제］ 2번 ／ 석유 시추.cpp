9579500689869ca66dcf0201e1d7e997b2b37785
Hyeonseung Park <hugh_eu@outlook.com>
2024-10-04 16:13:10 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 9.22 ms, Memory: 13.3 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
@@ -1,25 +1,26 @@
 #include <string>
+#include <queue>
 #include <vector>
 #include <map>
-#include <queue>
 
 using namespace std;
 
 void process_oil(int i, int j, vector<vector<int>> &land) {
     int qty = 0;
-    map<int, pair<int, int>> tops;
     queue<pair<int, int>> q;
     vector<int> vr = {-1, 1, 0, 0};
     vector<int> vc = {0, 0, -1, 1};
+    map<int, pair<int, int>> tops;
     
-    q.push(make_pair(i, j));
     land[i][j] = -1;
+    q.push(make_pair(i, j));
     
     while (!q.empty()) {
-        ++qty;
         int r = q.front().first;
         int c = q.front().second;
+        
         q.pop();
+        ++qty;
         
         for (int k = 0; k < 4; ++k) {
             if (
@@ -27,22 +28,17 @@ void process_oil(int i, int j, vector<vector<int>> &land) {
                 0 <= c + vc[k] && c + vc[k] < land[0].size() && 
                 land[r+vr[k]][c+vc[k]] == 1
             ) {
-                q.push(make_pair(r+vr[k], c+vc[k]));
                 land[r+vr[k]][c+vc[k]] = -1;
+                q.push(make_pair(r+vr[k], c+vc[k]));
             }
         }
-        if (r - 1 < 0) {
-            if (tops.count(c) == 0) {
-                tops[c] = make_pair(r, c);
-            } else if (tops[c].first > r) {
-                tops[c] = make_pair(r, c);
-            }
+        
+        if (r-1 < 0) {
+            if (tops.count(c) == 0) tops[c] = make_pair(r, c);
+            else if (tops[c].first > r) tops[c] = make_pair(r, c);
         } else if (land[r-1][c] == 0) {
-            if (tops.count(c) == 0) {
-                tops[c] = make_pair(r-1, c);
-            } else if (tops[c].first > r-1) {
-                tops[c] = make_pair(r-1, c);
-            }
+            if (tops.count(c) == 0) tops[c] = make_pair(r-1, c);
+            else if (tops[c].first > r-1) tops[c] = make_pair(r-1, c);
         }
     }
     
@@ -54,24 +50,16 @@ void process_oil(int i, int j, vector<vector<int>> &land) {
 int solution(vector<vector<int>> land) {
     int answer = 0;
     
-    for (int i = 0; i < land.size(); ++i) {
-        for (int j = 0; j < land[0].size(); ++j) {
-            if (land[i][j] == 1) {
-                process_oil(i, j, land);
-            }
-        }
+    for (int i = 0; i < land.size(); ++i) for (int j = 0; j < land[0].size(); ++j) {
+        if (land[i][j] == 1) process_oil(i, j, land);
     }
     
     for (int j = 0; j < land[0].size(); ++j) {
         int sum_oil = 0;
         for (int i = 0; i < land.size(); ++i) {
-            if (land[i][j] != -1) {
-                sum_oil += land[i][j];
-            }
+            if (land[i][j] != -1) sum_oil += land[i][j];
         }
-        if (answer < sum_oil) {
-            answer = sum_oil;
-        }
+        if (answer < sum_oil) answer = sum_oil;
     }
     
     return answer;
