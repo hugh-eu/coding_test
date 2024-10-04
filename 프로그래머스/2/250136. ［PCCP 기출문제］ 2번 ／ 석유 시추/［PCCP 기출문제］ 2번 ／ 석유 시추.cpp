6b41e90a87de20ffec660c41bdc3598d953553b8
Hyeonseung Park <hugh_eu@outlook.com>
2024-10-04 15:41:14 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 9.31 ms, Memory: 13.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.cpp
@@ -0,0 +1,78 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <queue>
+
+using namespace std;
+
+void process_oil(int i, int j, vector<vector<int>> &land) {
+    int qty = 0;
+    map<int, pair<int, int>> tops;
+    queue<pair<int, int>> q;
+    vector<int> vr = {-1, 1, 0, 0};
+    vector<int> vc = {0, 0, -1, 1};
+    
+    q.push(make_pair(i, j));
+    land[i][j] = -1;
+    
+    while (!q.empty()) {
+        ++qty;
+        int r = q.front().first;
+        int c = q.front().second;
+        q.pop();
+        
+        for (int k = 0; k < 4; ++k) {
+            if (
+                0 <= r + vr[k] && r + vr[k] < land.size() && 
+                0 <= c + vc[k] && c + vc[k] < land[0].size() && 
+                land[r+vr[k]][c+vc[k]] == 1
+            ) {
+                q.push(make_pair(r+vr[k], c+vc[k]));
+                land[r+vr[k]][c+vc[k]] = -1;
+            }
+        }
+        if (r - 1 < 0) {
+            if (tops.count(c) == 0) {
+                tops[c] = make_pair(r, c);
+            } else if (tops[c].first > r) {
+                tops[c] = make_pair(r, c);
+            }
+        } else if (land[r-1][c] == 0) {
+            if (tops.count(c) == 0) {
+                tops[c] = make_pair(r-1, c);
+            } else if (tops[c].first > r-1) {
+                tops[c] = make_pair(r-1, c);
+            }
+        }
+    }
+    
+    for (auto &iter : tops) {
+        land[iter.second.first][iter.second.second] = qty;
+    }
+}
+
+int solution(vector<vector<int>> land) {
+    int answer = 0;
+    
+    for (int i = 0; i < land.size(); ++i) {
+        for (int j = 0; j < land[0].size(); ++j) {
+            if (land[i][j] == 1) {
+                process_oil(i, j, land);
+            }
+        }
+    }
+    
+    for (int j = 0; j < land[0].size(); ++j) {
+        int sum_oil = 0;
+        for (int i = 0; i < land.size(); ++i) {
+            if (land[i][j] != -1) {
+                sum_oil += land[i][j];
+            }
+        }
+        if (answer < sum_oil) {
+            answer = sum_oil;
+        }
+    }
+    
+    return answer;
+}
