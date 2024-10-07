13dd8c184b11413795f1bd2e912cbe8eb293e419
Hyeonseung Park <hugh_eu@outlook.com>
2024-10-07 11:52:28 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_prefix) {
+    if (my_string.size() >= is_prefix.size() && my_string.substr(0, is_prefix.size()) == is_prefix) return 1;
+    else return 0;
+}
