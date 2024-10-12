95cd4223c10044a24ae47d5d4c745c7ab0677ea0
Hyeonseung Park <hugh_eu@outlook.com>
2024-10-13 03:39:24 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 3.78 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.cpp
@@ -1,28 +1,9 @@
-#include <string>
-#include <vector>
+#include <bitset>
 
 using namespace std;
 
-string get_binary(int i) {
-    string s = "";
-    while (i >= 1) {
-        s = to_string(i % 2) + s;
-        i = i / 2;
-    }
-    return s;
-}
-
 int solution(int n) {
-    string binary_n = get_binary(n);
-    int num_of_1_n = 0;
-    for (char c : binary_n) if (c == '1') ++num_of_1_n;
-    
-    int i = n + 1;
-    while (true) {
-        string binary_i = get_binary(i);
-        int num_of_1_i = 0;
-        for (char c : binary_i) if (c == '1') ++num_of_1_i;
-        if (num_of_1_i == num_of_1_n) return i;
-        ++i;
-    }
+    int i = bitset<20>(n).count();
+    while (bitset<20>(++n).count() != i);
+    return n;
 }
